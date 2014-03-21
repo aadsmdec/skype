ec70ef780cc4f2043fac39485ac43f469067cd78
Afonso <noreply>
2014-03-21 01:58:15 +0000

    Add component home

---
+++ ui/home.reel/home.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/home.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Home
+ * @extends Component
+ */
+exports.Home = Component.specialize(/** @lends Home# */ {
+    constructor: {
+        value: function Home() {
+            this.super();
+        }
+    }
+});

df594aacbb488a2d1155cf5f766240428dc34432
Afonso <noreply>
2014-03-21 03:14:19 +0000

    Update component /ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -22,7 +22,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
                 {name: "Adam", conversion: [
                     {source: "Adam", date: "12:23", message: "Hello there"},
                     {source: "Adam", date: "12:23", message: "How are you?"},
-                    {source: "Adam", date: "12:23", message: "Wanna go to the movies?}]},
+                    {source: "Adam", date: "12:23", message: "Wanna go to the movies?"}]},
                 {name: "Channel", conversion: [
                     {source: "Adam", date: "12:23", message: "Hello everyone"},
                     {source: "Anne", date: "13:23", message: "Sup?"},

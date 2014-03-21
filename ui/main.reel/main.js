d0157b01679b70530c92392f1f892a6835dac90c
Afonso <noreply>
2014-03-21 02:57:25 +0000

    Update component /ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -20,13 +20,13 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             
             this._recentConversations = [
                 {name: "Adam", conversion: [
-                    {source: "Adam", date: "12:23": message: "lskjdf lksdjf lskjdf lsdkjf "},
-                    {source: "Adam", date: "12:23": message: "slkdjhf lksjdhf lksjhd flkjsdf"},
-                    {source: "Adam", date: "12:23": message: "lkdfjg ldkfgjldkfj glkdfj glfd g"}]},
+                    {source: "Adam", date: "12:23", message: "lskjdf lksdjf lskjdf lsdkjf "},
+                    {source: "Adam", date: "12:23", message: "slkdjhf lksjdhf lksjhd flkjsdf"},
+                    {source: "Adam", date: "12:23", message: "lkdfjg ldkfgjldkfj glkdfj glfd g"}]},
                 {name: "Channel", conversion: [
-                    {source: "Adam", date: "12:23": message: "lskjdf lksdjf lskjdf lsdkjf "},
-                    {source: "Anne", date: "13:23": message: "slkdjhf lksjdhf lksjhd flkjsdf"},
-                    {source: "Heidi", date: "16:45": message: "lkdfjg ldkfgjldkfj glkdfj glfd g"}]}
+                    {source: "Adam", date: "12:23", message: "lskjdf lksdjf lskjdf lsdkjf "},
+                    {source: "Anne", date: "13:23", message: "slkdjhf lksjdhf lksjhd flkjsdf"},
+                    {source: "Heidi", date: "16:45", message: "lkdfjg ldkfgjldkfj glkdfj glfd g"}]}
             ];
         }
     }

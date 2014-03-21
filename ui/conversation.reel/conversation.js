9a808f344e34a9ff77df9933832b8e7d0f587532
Afonso <noreply>
2014-03-21 01:58:28 +0000

    Add component conversation

---
+++ ui/conversation.reel/conversation.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/conversation.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Conversation
+ * @extends Component
+ */
+exports.Conversation = Component.specialize(/** @lends Conversation# */ {
+    constructor: {
+        value: function Conversation() {
+            this.super();
+        }
+    }
+});

2200e627ef63dff26e02fdb10201d606f522cc40
Afonso <noreply>
2014-04-05 04:30:29 +0000

    Add component big-dom

---
+++ ui/big-dom.reel/big-dom.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/big-dom.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BigDom
+ * @extends Component
+ */
+exports.BigDom = Component.specialize(/** @lends BigDom# */ {
+    constructor: {
+        value: function BigDom() {
+            this.super();
+        }
+    }
+});
